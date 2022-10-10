903ad4ed1dff2335372192bcb659406b021f0a44
Sandeep Pudi <spudi84@gmail.com>
2022-10-10 15:27:35 -0700

    Method to rename the file on HDFS

--- src/get-insights.py
+++ src/get-insights.py
@@ -10,27 +10,24 @@ from pyspark.sql.functions import row_number, col
 from pyspark.sql.types import StructType,StructField, StringType, IntegerType, DecimalType
 # from pyspark.sql.types import ArrayType, DoubleType, BooleanType
 
-# os.environ["JAVA_HOME"]=os.path.join("C:\\Program Files (x86)\\Common Files\\Oracle\\Java\\javapath")
-
-# os.environ["PYSPARK_PYTHON"] = os.path.join("C:\\Users\\home\\Anaconda3\\python.exe")
 os.environ["PYSPARK_PYTHON"] = sys.executable
 os.environ['PYSPARK_DRIVER_PYTHON'] = sys.executable
 
-# "C:\Program Files (x86)\Java\jre1.8.0_341"
-
-access_key_id = "AKIA3EW3WXFA2T4QNQDR"
-secret_access_key = "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"
+access_key_id = "AKIA3EW3WXFASEQZGJGT"
+secret_access_key = "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"
 
 # spark = SparkSession.builder.appName("adobe_get_insights").config("spark.jars", "x.jar,y.jar").getOrCreate()
 spark = SparkSession.builder.appName("adobe_get_insights").getOrCreate()
 spark._jsc.hadoopConfiguration().set("fs.s3a.access.key", access_key_id)
 spark._jsc.hadoopConfiguration().set("fs.s3a.secret.key", secret_access_key)
 # spark._jsc.hadoopConfiguration().set("fs.s3a.impl","org.apache.hadoop.fs.s3a.S3AFileSystem")
+# spark._jsc.hadoopConfiguration().set("fs.s3a.impl","org.apache.hadoop.fs.s3native.NativeS3FileSystem")
 spark._jsc.hadoopConfiguration().set("com.amazonaws.services.s3.enableV4", "true")
 spark._jsc.hadoopConfiguration().set('spark.jars.packages', 'org.apache.hadoop:hadoop-aws:3.3.0')
 spark._jsc.hadoopConfiguration().set("fs.s3a.aws.credentials.provider","org.apache.hadoop.fs.s3a.BasicAWSCredentialsProvider")
 spark._jsc.hadoopConfiguration().set("fs.s3a.endpoint", "s3.us-west-2.amazonaws.com")
 # spark._jsc.hadoopConfiguration().set("fs.s3a.path.style.access", "false") 
+spark._jsc.hadoopConfiguration().set("fs.s3a.multiobjectdelete.enable", "false")
 
 csvDf = spark.read.csv("s3a://get-insights-poc/data/data.tsv",sep=r'\t', header=True)
 
@@ -87,6 +84,27 @@ def getSearchurls(csvDfRow):
         pass
     print(f"Exiting out of getSearchurls")
 
+def write_csv_with_specific_file_name(sc, df, path, filename):
+    df.repartition(1).write.options(header='True', delimiter=r'\t').mode("overwrite").format("csv").save(path)
+    try:
+        sc_uri = sc._gateway.jvm.java.net.URI
+        sc_path = sc._gateway.jvm.org.apache.hadoop.fs.Path
+        file_system = sc._gateway.jvm.org.apache.hadoop.fs.FileSystem
+        configuration = sc._gateway.jvm.org.apache.hadoop.conf.Configuration
+        fs = file_system.get(sc_uri("hdfs://localhost:8020"), configuration())
+        src_path = None
+        status = fs.listStatus(sc_path(path))
+        for fileStatus in status:
+            temp = fileStatus.getPath().toString()
+            if "part" in temp:
+                src_path = sc_path(temp)
+        dest_path = sc_path(path + filename)
+        if fs.exists(src_path) and fs.isFile(src_path):
+            fs.rename(src_path, dest_path)
+            fs.delete(src_path, True)
+    except Exception as e:
+        raise Exception("Error renaming the part file to {}:".format(filename, e))
+
 # filteredDf = csvDf.filter("event_list = 1" | "urlparse(referrer).path = '/search'")
 searchEngDF = csvDf.rdd.map(lambda x: getSearchurls(x)).toDF(["datetime", "ip","searchEngine","keyWord", "productName", "revenue"])
 
@@ -96,32 +114,38 @@ searchPurchaseDF = searchEngDF.filter(~ ( ( searchEngDF.searchEngine == 'None' )
     .withColumn("row_number",row_number().over(windowSpec))
 
 today = date.today().strftime("%Y-%m-%d")
-write_csv = today+"_SearchKeywordPerformance"
+# write_csv = "s3a://get-insights-poc/output/"+today
+write_csv = "output"
+output_file_name = "/"+today+"_SearchKeywordPerformance.tab"
+# write_csv = today+"_SearchKeywordPerformance"
 
-(searchPurchaseDF.alias("search").join(searchPurchaseDF.alias("purchase"), \
-    col("search.ip") == col("purchase.ip"), "inner") \
-        .select(col("search.ip"),col("search.searchEngine"),col("search.keyWord"), \
-            col("purchase.productName"),col("purchase.revenue"), \
-            col("search.datetime").alias("search_datetime"), col("purchase.datetime").alias("purchase_datetime"))\
-        .where(col("search.row_number") == col("purchase.row_number")-1)) \
-        .groupBy("searchEngine","keyWord").sum("revenue").withColumnRenamed("sum(revenue)","TotalRevenue") \
-            .write.options(header='True', delimiter=r'\t').mode("overwrite").format("csv") \
-                .csv("./"+write_csv)
+# (searchPurchaseDF.alias("search").join(searchPurchaseDF.alias("purchase"), \
+#     col("search.ip") == col("purchase.ip"), "inner") \
+#         .select(col("search.ip"),col("search.searchEngine"),col("search.keyWord"), \
+#             col("purchase.productName"),col("purchase.revenue"), \
+#             col("search.datetime").alias("search_datetime"), col("purchase.datetime").alias("purchase_datetime"))\
+#         .where(col("search.row_number") == col("purchase.row_number")-1)) \
+#         .groupBy("searchEngine","keyWord").sum("revenue").withColumnRenamed("sum(revenue)","TotalRevenue") \
+#         .coalesce(1) \
+#             .write.options(header='True', delimiter=r'\t').mode("overwrite").format("csv") \
+#                 .csv(write_csv)
+searchKWPerfDF = (searchPurchaseDF.alias("search").join(searchPurchaseDF.alias("purchase"), \
+                col("search.ip") == col("purchase.ip"), "inner") \
+                    .select(col("search.ip"),col("search.searchEngine"),col("search.keyWord"), \
+                        col("purchase.productName"),col("purchase.revenue"), \
+                        col("search.datetime").alias("search_datetime"), col("purchase.datetime").alias("purchase_datetime"))\
+                    .where(col("search.row_number") == col("purchase.row_number")-1)) \
+                    .groupBy("searchEngine","keyWord").sum("revenue").withColumnRenamed("sum(revenue)","TotalRevenue") \
+                    .coalesce(1)
+
+write_csv_with_specific_file_name(spark.sparkContext, searchKWPerfDF, write_csv, output_file_name)
 # schema = StructType() \
 #       .add("SearchEngine",StringType(),False) \
 #       .add("Keyword",StringType(),False) \
 #       .add("Revenue",DecimalType(),False)
 
-# prodSalesDF = csvDf.filter("event_list = 1").rdd.map(lambda x: getAllpurchases(x)).toDF(["purchaseDatetime","ip","productName","revenue"])
-# print(prodSalesDF.show())
+#  hdfs://localhost:8020/
 
-# windowSpec  = Window.partitionBy("ip").orderBy("date_time")
-
-# csvDf.withColumn("row_number",row_number().over(windowSpec)) \
-#     .show(truncate=False)
-
-
-# print(rdd2.collect())
 # clients = aws_clients()
 
 # s3_client = clients.s3_client
@@ -133,3 +157,4 @@ write_csv = today+"_SearchKeywordPerformance"
 
 # for ln in codecs.getreader('utf-8')(body):
 #     print(ln)
+
